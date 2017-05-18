c7a9778f7ae48cbad03e99e275047e9dcfd43576
vanitanaidu <vanita112003@gmail.com>
2017-05-17 20:27:48 -0400

    Update root path to be default upon login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || new_product_path
-  
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    devise_for :users
 
-
-  root 'welcome#home', as: 'welcome'
+  root 'welcome#home'
 
   resources :products, only: [:index, :new, :create]
   resources :daily_pick, only: [:index]
